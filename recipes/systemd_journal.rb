c54e29a4372625609b6619956509fc56cb65cd73
Nathan Williams <nathan@teamtreehouse.com>
2015-03-30 20:40:53 -0700

    systemd udp input, cops fix

--- recipes/systemd_journal.rb
+++ recipes/systemd_journal.rb
@@ -4,50 +4,42 @@
 #
 # Copyright 2015 Nathan Williams <nath.e.will@gmail.com>
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cookbook_file '/usr/share/heka/lua_decoders/systemd_journal.lua' do
-  source 'systemd_journal.lua'
-end
-
-heka_config 'systemd_journal_input' do
-  config({
-    "SystemdJournalInput" => {
-                 "type" => "ProcessInput",
-      "ticker_interval" => 0,
-              "timeout" => 0,
-              "decoder" => "SystemdJournalDecoder",
-              "command" => {
-        "0" => {
-           "bin" => "/usr/bin/journalctl",
-          "args" => ["-b", "-l", "-o", "json", "-f"]
-        }
-      }
+# Also requires 'ForwardToSyslog=yes' in journald.conf
+heka_config 'journal_syslog_input' do
+  config(
+    'journal_syslog_input' => {
+      'type' => 'UdpInput',
+      'address' => '/run/systemd/journal/syslog',
+      'net' => 'unixgram',
+      'decoder' => 'journal_syslog_decoder'
     }
-  })
+  )
   notifies :restart, 'service[hekad]', :delayed
 end
 
-heka_config 'systemd_journal_decoder' do
-  config({
-    "SystemdJournalDecoder" => {
-          "type" => "SandboxDecoder",
-      "filename" => "lua_decoders/systemd_journal.lua",
-        "config" => {
-                "type" => "systemd_journal",
-        "payload_keep" => false
+heka_config 'journal_syslog_decoder' do
+  config(
+    'journal_syslog_decoder' => {
+      'type' => 'SandboxDecoder',
+      'filename' => 'lua_decoders/rsyslog.lua',
+      'config' => {
+        'type' => 'journal_syslog',
+        'template' => '<%syslogfacility%>%timestamp% %syslogtag%%msg%',
+        'hostname_keep' => true
       }
     }
-  })
+  )
   notifies :restart, 'service[hekad]', :delayed
 end
