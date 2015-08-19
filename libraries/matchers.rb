10d113c65c5cb658612000253cffa5da3556f777
Nathan Williams <nathan@teamtreehouse.com>
2015-08-19 10:01:38 -0700

    add chefspec matcher

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,5 +1,7 @@
 # Matchers for ChefSpec 3
 if defined?(ChefSpec)
+  ChefSpec.define_matcher(:heka_config)
+
   def create_heka_config(check)
     ChefSpec::Matchers::ResourceMatcher.new(:heka_config, :create, check)
   end
