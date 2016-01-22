8df2b7eba137248fd9edc2e2e799ea204fa6d869
Nathan Williams <nathan@teamtreehouse.com>
2016-01-22 10:05:13 -0800

    rm deprecated requires

--- libraries/handlers.rb
+++ libraries/handlers.rb
@@ -16,8 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require "chef/resource/service"
-
 module Hekad
   class Handlers
     def conditionally_reload(run_context)
